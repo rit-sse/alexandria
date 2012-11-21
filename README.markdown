14f93f3d9a00edc4e911929323772905519368f4
Derek Erdmann <derek@derekerdmann.com>
2012-11-20 21:47:49 -0500

    Create empty Rails project

---
+++ README.markdown
@@ -0,0 +1,9 @@
+Alexandria
+==========
+
+Library Management System by the Society of Software Engineers
+
+Get started
+-----------
+1. Install Ruby
+2. Run `bundle install`
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alexandria::Application.config.secret_token = 'cdef780a394f6e3665c99c44cc8d2ceb5351aea776e8865f6c5c98af94752d414eb80f21502e35bf401b949d91fdf9bae69626b3ad23509962ca9bffb49d2e7c'
