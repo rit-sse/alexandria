51af0cbfcb127df677df53e7702eb4b2e08a6e9e
Derek Erdmann <derek@derekerdmann.com>
2012-12-29 22:16:28 -0500

    Stop redirecting build output to text files
    It's all logged in Jenkins anyway, so no need for more files

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,6 @@
 source ~/.bashrc 
 cd .
-[ -d jenkins ] && rm -rf jenkins
-pwd
-mkdir jenkins
-bundle install > jenkins/bundler.txt
-rake db:schema:load > jenkins/schema_load.txt
-SPEC_OPTS="--format html" rake rspec > jenkins/rspec.html
-CUCUMBER_FORMAT=html rake cucumber > jenkins/cucumber.html
+bundle install
+rake db:schema:load
+SPEC_OPTS="--format html" rake rspec
+CUCUMBER_FORMAT=html rake cucumber
