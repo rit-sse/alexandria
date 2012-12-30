94ccf01baa28642ee40353961fb2cf5921f0b33e
Derek Erdmann <derek@derekerdmann.com>
2012-12-29 21:44:53 -0500

    Add Jenkins build script

---
+++ jenkins/build.sh
@@ -0,0 +1,8 @@
+source ~/.bashrc 
+cd .
+[ -d jenkins ] && rm -rf jenkins
+mkdir jenkins
+bundle install > jenkins/bundler.txt
+rake db:schema:load > jenkins/schema_load.txt
+SPEC_OPTS="--format html" rake rspec > jenkins/rspec.html
+CUCUMBER_FORMAT=html rake cucumber > jenkins/cucumber.html
