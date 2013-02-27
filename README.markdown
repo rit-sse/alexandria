be200a3ad0bc5745ef1f949c5f5d493735902160
Dorrene Brown <dorreneb@gmail.com>
2013-02-27 18:01:30 -0500

    Can log out of the application

--- README.markdown
+++ README.markdown
@@ -7,3 +7,4 @@ Get started
 -----------
 1. Install Ruby
 2. Run `bundle install`
+3. If working on a branch with auth implemented, get the Google API keys from @dorreneb and insert them in the omniauth initializer (config/initializers/omniauth.rb)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Only permit omniauth accounts
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
