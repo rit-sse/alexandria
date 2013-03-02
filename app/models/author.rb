209aff1e92850502b710658ad891efaf95fd08e2
Chris Bentivenga <chris@chrisbenti.com>
2013-03-02 18:18:41 -0500

    Updates the author model with helper methods. Adds in coresponding tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,65 @@
 class Author < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :middle_initial
   has_and_belongs_to_many :book
+
+  validates_length_of :middle_initial, :maximum => 1, :allow_blank => false
+
+  def detault_values
+  	self.middle_initial ||= ""
+  end
+
+  ##
+  # Creates an Author when given a single string name. It DOES NOT save it.
+  #
+  # It parses the name, expecting it to be space deliniated into 3 parts.
+  # It expects strings in the format "<first> <last>" or "<first> <middle> <last>"
+  def self.create_with_name(name)
+  	return self.new(parse_name(name))
+  end
+
+  ##
+  # Finds an Author when given a single string name. 
+  #
+  # It parses the name, expecting it to be space deliniated into 3 parts.
+  # It expects strings in the format "<first> <last>" or "<first> <middle> <last>"
+  def self.find_with_name(name)
+  	name_parts = parse_name(name)
+
+  	return Author.where(first_name:     name_parts[:first_name],
+  						middle_initial: name_parts[:middle_initial],
+  						last_name:      name_parts[:last_name])[0]
+  end
+
+  ##
+  # Finds an author with the given name. If there is no such author, 
+  # creates one and returns that. If created, it DOES save it.
+  # 
+  # Uses the parsing rules: "<first> <last>" or "<first> <middle> <last>"
+  def self.find_or_create(name)
+  	author = find_with_name(name)
+  	if author == nil
+  		author = create_with_name(name)
+  		author.save
+  	end
+  	author
+  end
+
+  ##
+  # Parses the names into expected portions. All methods that parse
+  # a name for Author use this.
+  def self.parse_name(name)
+  	name_parts = name.split(' ')
+
+  	if name_parts.size == 2
+  		return {first_name: name_parts[0], 
+  				middle_initial: '',
+  				last_name: name_parts[1]}
+  	elsif name_parts.size == 3
+  		return {first_name: name_parts[0],
+  				middle_initial: name_parts[1].sub('.',''),
+  				last_name: name_parts[2]}
+  	else
+  		raise ArgumentError, "Argument should be in the form '<first> <last>' or '<first> <middle> <last>', space deliniated"	
+  	end
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Author do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a user properly with create_with_name" do
+  	author = Author.create_with_name("First Last")
+  	expect(author.first_name).to eq("First")
+  	expect(author.last_name).to eq("Last")
+
+  	author = Author.create_with_name("First M. Last")
+  	expect(author.first_name).to eq ("First")
+  	expect(author.middle_initial).to eq("M")
+	expect(author.last_name).to eq("Last")  	
+  end
+
+  it "finds a user properly with find_with_name" do
+  	first_name = "James"
+  	middle_initial = "T"
+  	last_name = "Kirk"
+
+	full_name = "#{first_name} #{middle_initial} #{last_name}"
+
+	author = Author.new(first_name: first_name, 
+					    middle_initial: middle_initial, 
+					    last_name: last_name)
+	author.save
+
+  	expect(Author.find_with_name(full_name)).to eq author
+  end
+
+  it "finds and creates users with find_or_create" do
+  	first_name = "James"
+  	middle_initial = "T"
+  	last_name = "Kirk"
+
+  	full_name = "#{first_name} #{middle_initial} #{last_name}"
+
+  	expect(Author.find_with_name(full_name)).to eq nil
+
+  	author = Author.find_or_create(full_name)
+	expect(Author.find_with_name(full_name)).to eq author
+	expect(Author.find_or_create(full_name)).to eq author
+  end
+
 end
