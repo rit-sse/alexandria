f60a84bd14705a2920f6485578144f928d41fe9c
Chris Bentivenga <chris@chrisbenti.com>
2013-04-10 15:24:04 -0400

    PICTURES

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,19 +12,38 @@ class Book < ActiveRecord::Base
   end
 
   def google_book
-    if not @google_book
-      begin
-    	 @google_book = GoogleBooks::API.search("isbn:#{self.ISBN}").first
-      rescue
-        @google_book = nil
-      end
+    # 
+
+    begin
+      return GoogleBooks::API.search("isbn:#{self.ISBN}").first      
+      Rails.cache.write(self, google_book)
+    rescue
+      return nil
     end
-    return @google_book
+  end
+
+  def thumbnail
+    if Rails.cache.read(self)
+       return Rails.cache.read(self)
+    end
+
+    g_book = google_book
+    thumbnail = ""
+    
+    if g_book
+      thumbnail = g_book.covers[:thumbnail]
+    end
+    
+    Rails.cache.write(self, thumbnail)
+    return thumbnail
+
   end
 
   def as_json(options = {})
     json = super(options)
     json[:authors] = self.author.as_json(only: [:first_name, :last_name, :middle_initial])
+    json[:thumbnail] = self.thumbnail
+
     json
   end
   
