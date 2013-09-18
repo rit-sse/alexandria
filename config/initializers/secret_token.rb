dd15e7536742fbc407c5ce1d61b1c93edc831d9d
kristenmills <kristen@kristen-mills.com>
2013-09-17 22:51:56 -0400

    Updated config files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Alexandria::Application.config.secret_token = 'cdef780a394f6e3665c99c44cc8d2ceb5351aea776e8865f6c5c98af94752d414eb80f21502e35bf401b949d91fdf9bae69626b3ad23509962ca9bffb49d2e7c'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alexandria::Application.config.secret_key_base = '19683d132e26d08a7fbce0a0b8d83a0d10fa15d1a3cfbce64c8a8f776fa1e6422655b284731b7c3c22de42e075260a388cf021e6e4ae55e0eb854127100943ba'
