d6288d196e61e3bc3f4551d5e460b1eae09c7c96
kristenmills <kristen@kristen-mills.com>
2013-09-18 09:49:36 -0400

    Authors updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
   def authenticate!
     unless current_user
       redirect_to root_path
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,24 +1,14 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
   # GET /authors
   # GET /authors.json
   def index
     @authors = Author.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @authors }
-    end
   end
 
   # GET /authors/1
   # GET /authors/1.json
   def show
-    @author = Author.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @author }
-    end
   end
 
   # GET /authors/new
@@ -34,18 +24,17 @@ class AuthorsController < ApplicationController
 
   # GET /authors/1/edit
   def edit
-    @author = Author.find(params[:id])
   end
 
   # POST /authors
   # POST /authors.json
   def create
-    @author = Author.new(params[:author])
+    @author = Author.new(author_params)
 
     respond_to do |format|
       if @author.save
         format.html { redirect_to @author, notice: 'Author was successfully created.' }
-        format.json { render json: @author, status: :created, location: @author }
+        format.json { render action: 'show', status: :created, location: @author }
       else
         format.html { render action: "new" }
         format.json { render json: @author.errors, status: :unprocessable_entity }
@@ -56,10 +45,8 @@ class AuthorsController < ApplicationController
   # PUT /authors/1
   # PUT /authors/1.json
   def update
-    @author = Author.find(params[:id])
-
     respond_to do |format|
-      if @author.update_attributes(params[:author])
+      if @author.update_attributes(author_params)
         format.html { redirect_to @author, notice: 'Author was successfully updated.' }
         format.json { head :no_content }
       else
@@ -72,7 +59,6 @@ class AuthorsController < ApplicationController
   # DELETE /authors/1
   # DELETE /authors/1.json
   def destroy
-    @author = Author.find(params[:id])
     @author.destroy
 
     respond_to do |format|
@@ -80,4 +66,15 @@ class AuthorsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :middle_initial)
+    end
 end
