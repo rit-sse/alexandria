6682492aa1ff29f5a6a97c774a03413746c28370
Nicolas McCurdy <thenickperson@gmail.com>
2013-09-18 17:36:14 -0400

    Fix using the wrong instance variable names in the set_* methods of controllers

--- app/controllers/strikes_controller.rb
+++ app/controllers/strikes_controller.rb
@@ -64,7 +64,7 @@ class StrikesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_strike
-      @author = Strike.find(params[:id])
+      @strike = Strike.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
