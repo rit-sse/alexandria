23c105723dac4ee7236ea48dbbe0478237fa135b
kristenmills <kristen@kristen-mills.com>
2013-09-21 09:21:01 -0400

    Added user and book factories

---
+++ spec/factories/book.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :book do
+    isbn 9780199273133
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email 'user@email.com'
+    password  'password'
+  end
+end
