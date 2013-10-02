b8ed29a93a5aa8a2a9b48b0e407b1b9b01b644c5
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-02 14:12:58 -0400

    Fix several recent rubocop offenses

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,12 +1,12 @@
 class RootController < ApplicationController
   def index
-    if not Book.all.empty?
+    if !Book.all.empty?
       @books = (0..2).to_a.map do |i|
         Book.featured_book
       end
       @books = @books.select{|i| !i.nil?}
     else
-      @books = Array.new
+      @books = []
     end
   end
 end
--- app/models/concerns/lccable.rb
+++ app/models/concerns/lccable.rb
@@ -10,26 +10,24 @@ module Lccable
   LCC_REGEX = /^(?<aclass>[A-Z]{1,3})(?<nclass>\d{1,4})(\ ?)(\.(?<dclass>\d{1,3}))?(?<date>\ [A-Za-z0-9]{1,4}\ )?([\ \.](?<c1>[A-Z][0-9]{1,4}))(\ (?<c1d>[A-Za-z0-9]{0,4}))?(\.?(?<c2>[A-Z][0-9]{1,4}))?(\ (?<e8>\w*)\ ?)?(\ (?<e9>\w*)\ ?)?(\ (?<e10>\w*)\ ?)?/
 
   def get_lcc
-    begin
-      ZOOM::Connection.open('z3950.loc.gov', 7090) do |conn|
-        conn.database_name = 'VOYAGER'
-        conn.preferred_record_syntax = 'USMARC'
-        conn.search("@attr 1=7 #{self.isbn}").each_record do |item|
-          MARC::Reader.new(StringIO.new(item.raw)).each do |rec|
-            unless rec['050'].nil?
-              if rec['050']['b'] =~ /\..*/
-                second_part = rec['050']['b'][1..-1]
-              else
-                second_part = rec['050']['b']
-              end
-              self.lcc = normalize("#{rec['050']['a']} #{second_part}")
+    ZOOM::Connection.open('z3950.loc.gov', 7090) do |conn|
+      conn.database_name = 'VOYAGER'
+      conn.preferred_record_syntax = 'USMARC'
+      conn.search("@attr 1=7 #{self.isbn}").each_record do |item|
+        MARC::Reader.new(StringIO.new(item.raw)).each do |rec|
+          unless rec['050'].nil?
+            if rec['050']['b'] =~ /\..*/
+              second_part = rec['050']['b'][1..-1]
+            else
+              second_part = rec['050']['b']
             end
+            self.lcc = normalize("#{rec['050']['a']} #{second_part}")
           end
         end
       end
-    rescue
-      puts "Connection Lost"
     end
+  rescue
+    puts "Connection Lost"
   end
 
   def normalize(callno)
