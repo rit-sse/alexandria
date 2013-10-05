adc6b7145dfc324e01545469a47253288a6059f4
kristenmills <kristen@kristen-mills.com>
2013-10-05 13:39:54 -0400

    Added mailers spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user{n}@example.com" }
     password  'password'
+    user_name 'First Last'
   end
 end
--- spec/mailers/checkout_mailer_spec.rb
+++ spec/mailers/checkout_mailer_spec.rb
@@ -1,5 +1,56 @@
 require "spec_helper"
 
-describe CheckoutMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe CheckoutMailer, solr: true do
+  let(:user) { create(:user) }
+  let(:book) { Book.add_by_isbn('9780201767308') }
+  let(:checkout) do
+    Checkout.new(
+                checked_out_at: Date.today,
+                due_date: Date.today + 1.week,
+                book_id: book.id,
+                patron_id: user.id
+              )
+  end
+
+  shared_examples_for 'a checkout mailer' do
+    it 'renders the reciever email' do
+      expect(mail.to).to include(user.email)
+    end
+    it 'assigns @user' do
+      expect(mail.body.encoded).to match(user.user_name)
+      expect(mail.body.encoded).to match(/#{user.strikes.count}\s*strikes/)
+    end
+
+    it 'assigns @book' do
+      expect(mail.body.encoded).to match(book.title)
+    end
+
+    it 'assigns @checkout' do
+      expect(mail.body.encoded).to match(checkout.due_date.strftime('%B %d, %Y'))
+    end
+  end
+
+  describe 'overdue_book' do
+    let(:mail) { CheckoutMailer.overdue_book(checkout) }
+
+    it_behaves_like 'a checkout mailer'
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('You have a book overdue.')
+    end
+
+    it 'assigns @user' do
+      expect(mail.body.encoded).to match(/#{user.strikes.count}\s*strikes/)
+    end
+  end
+
+  describe 'reminders' do
+    let(:mail) { CheckoutMailer.reminder(checkout) }
+
+    it_behaves_like 'a checkout mailer'
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('You have a book due soon.')
+    end
+  end
 end
