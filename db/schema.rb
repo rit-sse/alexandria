feaa2356cb4078b9834096ef14925d4f1a47c2b2
kristenmills <kristen@kristen-mills.com>
2013-10-07 18:12:40 -0400

    Removed UUID

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131006203914) do
+ActiveRecord::Schema.define(version: 20131007200654) do
 
   create_table "author_books", force: true do |t|
     t.integer  "author_id"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20131006203914) do
     t.string   "isbn"
     t.string   "title"
     t.date     "publish_date"
-    t.string   "UUID"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "subtitle"
