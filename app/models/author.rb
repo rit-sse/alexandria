6c58cf5f4513190ffe259b7587bfd3e575fe4210
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-07 13:14:35 -0400

    Rename name_parts to name

--- app/models/author.rb
+++ app/models/author.rb
@@ -39,11 +39,11 @@ class Author < ActiveRecord::Base
   # It parses the name, expecting it to be space deliniated into 3 parts.
   # It expects strings in the format "<first> <last>" or "<first> <middle> <last>"
   def self.find_with_name(name)
-    name_parts = parse_name(name)
+    name = parse_name(name)
 
-    Author.where(first_name:   name_parts[:first_name],
-                 middle_initial: name_parts[:middle_initial],
-                 last_name:   name_parts[:last_name])[0]
+    Author.where(first_name:   name[:first_name],
+                 middle_initial: name[:middle_initial],
+                 last_name:   name[:last_name])[0]
   end
 
   ##
@@ -65,17 +65,17 @@ class Author < ActiveRecord::Base
   # Parses the names into expected portions. All methods that parse
   # a name for Author use this.
   def self.parse_name(name)
-    name_parts = name.split(' ')
-    case name_parts.size
+    name = name.split(' ')
+    case name.size
     when 2
-      format_name(name_parts[0], '', name_parts[1])
+      format_name(name[0], '', name[1])
     when 3
-      format_name(name_parts[0], name_parts[1].sub('.', ''), name_parts[2])
+      format_name(name[0], name[1].sub('.', ''), name[2])
     when 4
-      if ['III', 'Jr', 'Jr.'].include? name_parts[3]
-        format_name(name_parts[0], name_parts[1].sub('.', ''), name_parts[2])
+      if ['III', 'Jr', 'Jr.'].include? name[3]
+        format_name(name[0], name[1].sub('.', ''), name[2])
       else
-        format_name(name_parts[0], "#{name_parts[1]} #{name_parts[2]}".sub('.', ''), name_parts[3])
+        format_name(name[0], "#{name[1]} #{name[2]}".sub('.', ''), name[3])
       end
     else
       raise ArgumentError, "Error with #{name} argument should be in the form '<first> <last>' or '<first> <middle> <last>', space deliniated"
