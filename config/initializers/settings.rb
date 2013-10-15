47674f9057cef4adf4471d3dd8f61f5776dcb2be
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-15 17:49:06 -0400

    Add the strikes_for_ban setting

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -3,3 +3,4 @@
 Rails.configuration.reservation_period = 1.week
 Rails.configuration.checkout_period    = 1.week
 Rails.configuration.remind_before      = 3.days
+Rails.configuration.strikes_for_ban    = 3
