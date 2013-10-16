1da37f7c8a8d6e9afe64621ded2e80134f7b9a64
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-16 02:58:05 -0400

    Add the shelves setting (currently empty)

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -4,3 +4,7 @@ Rails.configuration.reservation_period = 1.week
 Rails.configuration.checkout_period    = 1.week
 Rails.configuration.remind_before      = 3.days
 Rails.configuration.strikes_for_ban    = 3
+
+# TODO: Replace this with an Array of Ranges of Strings. Each Range should
+# represent the first and last book codes for the given shelf.
+Rails.configuration.shelves = []
