42f6d55e75c9710f8d9d818f1b919059b78fd005
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-16 11:45:57 -0400

    Revert "Use format Strings instead of String interpolation in Lccable#normalize"
    This reverts commit 842acb57bcba5bd78b9662bb42436f4fbd26c906.
    
    Conflicts:
    	app/models/concerns/lccable.rb

--- app/models/concerns/lccable.rb
+++ app/models/concerns/lccable.rb
@@ -41,14 +41,14 @@ module Lccable
   def normalize(callno)
     cp = LCC_REGEX.match(callno)
     out = cp[:aclass] + cp[:nclass]
-    out += '.%s' % cp[:dclass] if cp[:dclass].present?
-    out += ' %s ' % cp[:dclass] if cp[:date].present?
-    out += '.%s' % cp[:c1]
-    out += ' %s ' % cp[:c1d] if cp[:c1d].present?
-    out += ' %s' % cp[:c2] if cp[:c2].present?
-    out += ' %s' % cp[:e8] if cp[:e8].present?
-    out += ' %s' % cp[:e9] if cp[:e9].present?
-    out += ' %s' % cp[:e10] if cp[:e10].present?
+    out += ".#{cp[:dclass]}" if cp[:dclass].present?
+    out += " #{cp[:dclass]} " if cp[:date].present?
+    out += ".#{cp[:c1]}"
+    out += " #{cp[:c1d]} " if cp[:c1d].present?
+    out += " #{cp[:c2]}" if cp[:c2].present?
+    out += " #{cp[:e8]}" if cp[:e8].present?
+    out += " #{cp[:e9]}" if cp[:e9].present?
+    out += " #{cp[:e10]}" if cp[:e10].present?
     out
   end
 
