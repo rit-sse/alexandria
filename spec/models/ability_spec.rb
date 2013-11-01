4572d1ed1fda8a3f15f7ce2b2f203d6730649a9b
kristenmills <kristen@kristen-mills.com>
2013-11-01 14:41:47 -0400

    Added ability specs for librarian

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -6,7 +6,7 @@ describe Ability do
   let(:patron) { Ability.new(create(:patron)) }
   let(:none) { Ability.new }
 
-  describe :technical_admin do
+  describe 'technical admin' do
     let(:technical_admin) { Ability.new(create(:technical_admin)) }
 
     it 'can read authors' do
@@ -17,6 +17,7 @@ describe Ability do
     end
     it 'can read books' do
       expect(technical_admin.can?(:read, Book)).to be_true
+      expect(technical_admin.cannot?(:put_away, Book)).to be_true
       expect(technical_admin.cannot?(:create, Book)).to be_true
       expect(technical_admin.cannot?(:update, Book)).to be_true
       expect(technical_admin.cannot?(:destroy, Book)).to be_true
@@ -27,6 +28,7 @@ describe Ability do
       expect(technical_admin.cannot?(:create, Checkout)).to be_true
       expect(technical_admin.cannot?(:update, Checkout)).to be_true
       expect(technical_admin.cannot?(:destroy, Checkout)).to be_true
+      expect(technical_admin.cannot?(:check_in, Checkout)).to be_true
     end
 
     it 'can manage reservations' do
@@ -47,6 +49,52 @@ describe Ability do
       expect(technical_admin.can?(:create, User)).to be_true
       expect(technical_admin.can?(:update, User)).to be_true
       expect(technical_admin.can?(:destroy, User)).to be_true
+    end
+  end
+
+  describe 'librarain ' do
+    let(:librarian) { Ability.new(create(:librarian)) }
+
+    it 'can manage authors' do
+      expect(librarian.can?(:read, Author)).to be_true
+      expect(librarian.can?(:create, Author)).to be_true
+      expect(librarian.can?(:update, Author)).to be_true
+      expect(librarian.can?(:destroy, Author)).to be_true
+    end
+    it 'can manage books' do
+      expect(librarian.can?(:read, Book)).to be_true
+      expect(librarian.can?(:put_away, Book)).to be_true
+      expect(librarian.can?(:create, Book)).to be_true
+      expect(librarian.can?(:update, Book)).to be_true
+      expect(librarian.can?(:destroy, Book)).to be_true
+    end
+
+    it 'can manage checkouts' do
+      expect(librarian.can?(:read, Checkout)).to be_true
+      expect(librarian.can?(:create, Checkout)).to be_true
+      expect(librarian.can?(:update, Checkout)).to be_true
+      expect(librarian.can?(:destroy, Checkout)).to be_true
+      expect(librarian.can?(:check_in, Checkout)).to be_true
+    end
+
+    it 'can manage reservations' do
+      expect(librarian.can?(:read, Reservation)).to be_true
+      expect(librarian.can?(:create, Reservation)).to be_true
+      expect(librarian.can?(:update, Reservation)).to be_true
+      expect(librarian.can?(:destroy, Reservation)).to be_true
+    end
+
+    it 'can manage strikes' do
+      expect(librarian.can?(:read, Strike)).to be_true
+      expect(librarian.can?(:create, Strike)).to be_true
+      expect(librarian.can?(:update, Strike)).to be_true
+      expect(librarian.can?(:destroy, Strike)).to be_true
+    end
+    it 'can manage users' do
+      expect(librarian.can?(:read, User)).to be_true
+      expect(librarian.can?(:create, User)).to be_true
+      expect(librarian.can?(:update, User)).to be_true
+      expect(librarian.can?(:destroy, User)).to be_true
     end
   end
 end
