4b9db0ec563da6b1a54a6a247de30538c87cd02f
kristenmills <kristen@kristen-mills.com>
2013-12-04 12:54:45 -0500

    Updated the homepage. Pretty much the same as used to be before the giant buttons...just implemented more reasonably

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,13 +2,6 @@
 class RootController < ApplicationController
   skip_authorization_check
   def index
-    if !Book.all.empty?
-      @books = (0..2).to_a.map do |i|
-        Book.featured_book
-      end
-      @books = @books.select { |i| !i.nil? }
-    else
-      @books = []
-    end
+    @books = Book.all.sample(3)
   end
 end
